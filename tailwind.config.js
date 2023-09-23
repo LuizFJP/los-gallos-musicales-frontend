749928ba227efadea9a0a850137446e207c8da77
gusdev0258 <16326176760@edu.udesc.br>
2023-09-23 12:25:55 -0300

    add favicon and fontfaces

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{html,ts,js,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      'mono': ['IBM Plex mono', 'monospace'],
+      'cursive': ['Cinzel Decorative', 'cursive'],
+      'serif': ['Merriweather', 'serif', 'ui-serif', 'Georgia'],
+      'sans': ['IBM Plex Sans', 'sans-serif', 'ui-sans-serif', 'system-ui'],
+    },
     extend: {},
   },
   plugins: [],
