31c34b36baafb75ae6003e77fba465ef38949a1a
gusdev0258 <16326176760@edu.udesc.br>
2023-09-23 18:56:42 -0300

    feat: refactored and styled home page

--- src/presentation/components/RoomList.tsx
+++ src/presentation/components/RoomList.tsx
@@ -1,20 +1,24 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 export type RoomListProps = {
   rooms: string[];
-}
+};
 
 const RoomList = (props: RoomListProps) => {
-
   return (
-    <>
+    <div className="w-full h-96 flex flex-col justify-center ">
+      <h1 className="font-sans text-3xl">Salas Criadas</h1>
+      <p className="font-serif text-sm">Escolha uma para jogar</p>
+    <ul className="h-80 p-8 mt-4 rounded-md flex items-start justify-center gap-6 flex-wrap border-4 border-solid border-gray-100 overflow-y-scroll">
       {props.rooms.map((room, id) => (
-        <Link to={`/room/${room}`}>
-          <div key={id}>{room}</div>
-        </Link>
+        <li key={id} className="px-4 py-2 border-2 border-solid border-gray-100 rounded-md hover:bg-gray-200 hover:cursor-pointer hover:text-gray-800 mt-4">
+          <Link to={`/room/${room}`} className="p-2">
+            <span>{room}</span>
+          </Link>
+        </li>
       ))}
-    </>
+    </ul>
+    </div>
   );
 };
 
--- src/presentation/pages/Home.tsx
+++ src/presentation/pages/Home.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
 import RoomList from "../components/RoomList";
 import { createRoom, loadRooms } from "../../infra/http/request-room";
+import Logo from "../../assets/galinho.png";
+import Avatar from "../../assets/avatars/avatar_01.png";
+import { MdCreate } from "react-icons/md";
 
 const App: React.FC = () => {
   const [room, setRoom] = useState<string>("");
   const [roomList, setRoomList] = useState<string[]>([]);
+  const [username, setUsername] = useState<string>("");
 
   useEffect(() => {
     loadRooms().then((rooms) => {
@@ -22,21 +26,91 @@ const App: React.FC = () => {
     createRoom(room);
   }
 
+  const handleUser = () => {};
+
   return (
-    <main className="container mx-auto p-4">
-      <header></header>
-      <section>
-        <label htmlFor="roomName">
-          Nome da sala
-          <input
-            name="roomName"
-            type="text"
-            value={room}
-            onChange={handleRoomClick}
-          />
-        </label>
-        <input type="button" value="enviar" onClick={handleToggleClick} />
-        {roomList && <RoomList rooms={roomList} />}
+    <main className="container mx-auto px-4 items-center justify-center flex-wrap">
+      <header className="flex justify-center items-center h-48 flex-wrap flex-1">
+        <div className="w-28 h-28 flex items-center justify-center">
+          <img src={Logo} alt="galinho lgcm" />
+        </div>
+        <div className="">
+          <h1 className="font-cursive text-5xl w-80 text-center">
+            Los Gallos Musicales
+          </h1>
+          <p className="text-center font-serif text-sm">
+            Divirta-se com muitas musicas
+          </p>
+        </div>
+      </header>
+      <section className="flex items-end justify-center flex-wrap gap-12">
+        <div className="mt-12 flex flex-col flex-wrap items-center justify-center">
+          <div className="mb-8">
+            <div className="avatar-container flex flex-col items-center justify-center relative">
+              <img
+                src={Avatar}
+                alt=""
+                className="selected-avatar rounded-full object-cover h-40 w-40 borer-solid border-4 border-gray-50"
+              />
+              <MdCreate
+                size="32"
+                className="absolute bottom-0 right-0 rounded-full text-gray-50 bg-gray-600 border-solid border-2 border-gray-50"
+              />
+            </div>
+            <p className="font-mono text-sm mt-2 font-bold text-gray-100">
+              Escolha seu avatar
+            </p>
+          </div>
+          <div className="action-container">
+            <form action="" className="flex flex-col">
+              <label
+                htmlFor="username"
+                className="flex flex-col gap-1 mb-2 font-mono font-bold text-lg"
+              >
+                Apelido
+                <input
+                  name="username"
+                  type="text"
+                  value={username}
+                  onChange={handleUser}
+                  className="font-sans font-normal text-gray-950 px-2 py-1 rounded-md focus:outline-none focus:ring-2 focus:ring-amber-400 focus:border-transparent"
+                />
+              </label>
+              <label
+                htmlFor="roomName"
+                className="flex flex-col gap-1 font-mono font-bold text-lg"
+              >
+                Nome da sala
+                <input
+                  name="roomName"
+                  type="text"
+                  value={room}
+                  onChange={handleRoomClick}
+                  className="font-sans font-normal text-gray-950 px-2 py-1 rounded-md focus:outline-none focus:ring-2 focus:ring-amber-400 focus:border-transparent"
+                />
+              </label>
+              <div className="flex gap-4 flex-wrap items-center justify-center font-mono font-bold">
+                <button
+                  type="button"
+                  value="enviar"
+                  onClick={handleToggleClick}
+                  className="bg-gray-600 text-gray-200 rounded-sm px-8 py-2 mt-4"
+                >
+                  Jogar
+                </button>
+                <button
+                  type="button"
+                  value="enviar"
+                  onClick={handleToggleClick}
+                  className="bg-gray-800 text-gray-200 rounded-sm px-8 py-2 mt-4"
+                >
+                  Criar Sala
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div>{roomList && <RoomList rooms={roomList} />}</div>
       </section>
     </main>
   );
