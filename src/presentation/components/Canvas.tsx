12042e4b008516089dc411f239bd703b761e8913
gusdev0258 <16326176760@edu.udesc.br>
2023-09-23 11:31:03 -0300

    refactored canvas and room

--- src/presentation/components/Canvas.tsx
+++ src/presentation/components/Canvas.tsx
@@ -1,13 +1,104 @@
-import React from "react";
+import React, { useEffect } from "react";
 import type { RefObject } from "react";
 import { WebSocketProtocol } from "../../infra/protocols/websocket-protocol";
 
 export type CanvasProps = {
   socket: WebSocketProtocol;
   canvasRef: RefObject<HTMLCanvasElement>;
+  handleCanvasDataTransmission: (x: number, y: number) => void;
+  roomName?: string;
 };
 
 export const Canvas = (props: CanvasProps) => {
+  function drawCircle(
+    context: CanvasRenderingContext2D | null,
+    x: number,
+    y: number
+  ) {
+    if (!context) return;
+
+    context.fillStyle = "blue";
+    context.beginPath();
+    context.arc(x, y, 10, 0, Math.PI * 2);
+    context.fill();
+  }
+
+  async function startDraw(context: CanvasRenderingContext2D | null) {
+    const res = await fetch(
+      `http://localhost:8100/room/?name=${props.roomName}`
+    );
+    const draw = await res.json();
+    var img = new Image();
+    img.onload = () => context?.drawImage(img, 0, 0);
+    img.src = draw;
+    console.log(draw);
+  }
+
+  useEffect(() => {
+    const canvas = props.canvasRef.current;
+    if (!canvas) return;
+
+    const context = canvas.getContext("2d");
+
+    if (!context) {
+      console.error("Contexto 2D não suportado pelo navegador.");
+      return;
+    }
+
+    startDraw(context);
+
+    let isDrawing = false;
+
+    props.socket.onDraw(`${props.roomName} draw`, (data: any) => {
+      drawCircle(context, data.x, data.y);
+    });
+
+    // Manipuladores de eventos para desenhar ao clicar e arrastar
+    function handleMouseDown(event: MouseEvent) {
+      if (!canvas) return;
+
+      isDrawing = true;
+      drawCircle(
+        context,
+        event.clientX - canvas.offsetLeft,
+        event.clientY - canvas.offsetTop
+      );
+      props.handleCanvasDataTransmission(
+        event.clientX - canvas.offsetLeft,
+        event.clientY - canvas.offsetTop
+      );
+    }
+
+    function handleMouseMove(event: MouseEvent) {
+      if (!isDrawing || !canvas) return;
+      drawCircle(
+        context,
+        event.clientX - canvas.offsetLeft,
+        event.clientY - canvas.offsetTop
+      );
+      props.handleCanvasDataTransmission(
+        event.clientX - canvas.offsetLeft,
+        event.clientY - canvas.offsetTop
+      );
+    }
+
+    function handleMouseUp() {
+      isDrawing = false;
+    }
+
+    // Adicionando ouvintes de eventos ao canvas
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mouseup", handleMouseUp);
+
+    // Removendo ouvintes de eventos quando o componente é desmontado
+    return () => {
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [drawCircle]);
+
   const connect = () => {
     props.socket.connect();
   };
