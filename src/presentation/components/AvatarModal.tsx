1bd92efed85e2c32feda7c73d62b48f4cffa130e
gusdev0258 <16326176760@edu.udesc.br>
2023-10-07 11:32:33 -0300

    fix homescreen

--- src/presentation/components/AvatarModal.tsx
+++ src/presentation/components/AvatarModal.tsx
@@ -4,18 +4,17 @@ import { useState } from "react";
 
 export type AvatarModalProps = {
   isOpen: boolean;
-  onClose?: () => void;
   avatarList: Avatar[];
 };
 
 export const AvatarModal = (props: AvatarModalProps) => {
-  const [selectedAvatar, setSelectedAvatar] = useState<Avatar>();
+  const [selectedAvatar, setSelectedAvatar] = useState<string>();
   return (
     props.isOpen && (
       <section className="avatar-modal-container z-10 w-full h-full absolute flex items-center justify-center">
         <div className="avatar-modal bg-gray-300 w-[648px] h-[474px] rounded-xl p-4 relative flex items-center justify-center flex-col">
           <header className="w-full">
-            <div className="text-gray-950 w-full flex justify-end p-2 cursor-pointer" onClick={}>
+            <div className="text-gray-950 w-full flex justify-end p-2 cursor-pointer">
               <MdClose size={32} />
             </div>
             <div className="avatar-header flex items-center justify-center text-gray-900 text-lg xl:text-xl p-4 mb-2">
@@ -33,6 +32,10 @@ export const AvatarModal = (props: AvatarModalProps) => {
                       src={`data:${avatar.mimetype};base64,${avatar.base64}`}
                       alt=""
                       className="avatar rounded-full object-cover h-28 w-28 borer-solid border-4 border-gray-50 cursor-pointer hover:border-blue-600"
+                      onClick={(event: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+                        const element = event.target as HTMLImageElement;
+                        setSelectedAvatar(element.src);
+                      } }
                     />
                   </li>
                 ))}
