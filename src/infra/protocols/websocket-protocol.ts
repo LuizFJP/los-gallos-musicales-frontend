71b7f756b5912185f2a9d6e9de0672a94cbfa551
gusdev0258 <16326176760@edu.udesc.br>
2023-10-12 23:24:07 -0300

    creating chat

--- src/infra/protocols/websocket-protocol.ts
+++ src/infra/protocols/websocket-protocol.ts
@@ -2,5 +2,5 @@ export interface WebSocketProtocol {
   connect(): void;
   disconnect(): void;
   emitData(dataName: string, data: any): void;
-  onDraw(name: string, callback: any): void;
+  on(name: string, callback: any): void;
 }
--- src/presentation/components/chat/talk/talk-chat.tsx
+++ src/presentation/components/chat/talk/talk-chat.tsx
@@ -1,4 +1,10 @@
+import { useRef } from "react";
+import { SocketConnection } from "../../../../infra/websocket/websocket";
+import { MdSend } from "react-icons/md";
+
 const TalkChat = () => {
+  const socket = new SocketConnection();
+  const inputRef = useRef<HTMLInputElement>();
   const messagesMock = [
     {
       username: 'josefino',
@@ -23,8 +29,23 @@ const TalkChat = () => {
       text: 'Limão'
     },
   ]
+
+  const sendTextMessage = (textMessage) => {
+    socket.emitData('talk-chat-message', textMessage);
+    socket.on('talk-chat-message', (message) => { 
+      console.log(message);
+    });
+
+  }
+
+  const handleSendMessage = (event) => {
+    event.preventDefault();
+    sendTextMessage(inputRef.current?.value);
+    console.log(inputRef);
+  }
+
   return (
-    <section className="h-full border-l-2 border-solid border-gray-50 p-4">
+    <section className="p-2">
       <div className="answer-chat-container">
         <ul className="flex flex-col gap-2 justify-start">
           {messagesMock.map((message, index) => (
@@ -35,9 +56,10 @@ const TalkChat = () => {
           ))}
         </ul>
       </div>
-      <div className="answer-container mt-3">
-        <input type="text" className="answer-input p-2 rounded-md" placeholder="Converse aqui" />
-      </div>
+      <form className="answer-container mt-3 flex gap-2" onSubmit={handleSendMessage}>
+        <input type="text" className="talk-input p-2 rounded-md flex w-60" placeholder="Converse aqui" ref={inputRef} />
+        <button type="submit" className="flex items-center justify-center gap-2 text-sm border-solid border-2 border-gray-50 py-2 px-4 rounded-md hover:bg-gray-300 hover:text-gray-600 transition-colors">Enviar <MdSend size={16} /></button>
+      </form>
     </section>
   )
 }
