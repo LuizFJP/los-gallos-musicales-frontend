972eb13c2c2e2dca81cb846d76a4ac1a2b0b64ab
gusdev0258 <16326176760@edu.udesc.br>
2023-10-22 16:04:39 -0300

    create answer chat

--- src/presentation/components/chat/answer/answer-chat.tsx
+++ src/presentation/components/chat/answer/answer-chat.tsx
@@ -1,74 +1,69 @@
-import { useRef } from "react";
-import { startSocket } from "../../../../infra/websocket/websocket";
-import { MdCreate, MdSend } from "react-icons/md";
-import { BtnPrimary } from "../../button/primary/btn-primary";
+import { useEffect, useRef, useState } from "react";
 
 import "./answer-chat.scss";
+import { useSearchParams } from "react-router-dom";
 import { chatProps } from "../chat";
 
 export interface chatChildrenProps extends chatProps {}
 
-const AnswerChat = ({ socket, username }: chatChildrenProps) => {
-  const inputRef = useRef<HTMLInputElement>();
+interface Message {
+  sender: string;
+  text: string;
+}
 
-  const answersMock = [
-    {
-      username: "josefino",
-      answer: "Maçã",
-    },
-    {
-      username: "pluton",
-      answer: "Maracujá",
-    },
-    {
-      username: "zefron",
-      answer: "Banana",
-    },
-    {
-      username: "florencio",
-      answer: "Melância",
-    },
-    {
-      username: "Josefino",
-      answer: "Melão",
-    },
-    {
-      username: "japa",
-      answer: "Laranja",
-    },
-    {
-      username: "zoro",
-      answer: "Limão",
-    },
-  ];
+const AnswerChat = ({ socket, username }: chatChildrenProps) => {
+  const inputRef = useRef<HTMLInputElement | null>();
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const [searchParams] = useSearchParams();
+  const roomName = searchParams.get("name");
+
+  const handleNewMessage = (message) => {
+    console.log('recebeu mensagem', message)
+    setMessages((prevMessages) => [...prevMessages, message]);
+  };
 
   const sendTextMessage = (textMessage) => {
-    socket.on("talk-chat-message", (message) => {
-      console.log(message);
-    });
+    console.log("mandou mensage", messages)
+    socket.emit("answer-chat-message", roomName, textMessage);
+    setMessages((prevMessages) => [...prevMessages, textMessage]);
+    inputRef.current!.value = "";
   };
+
+  useEffect(() => {
+    console.log('entrou no useEffect')
+    socket.on("answer-chat-message", handleNewMessage);
+  }, []);
 
   const handleSendMessage = (event) => {
     event.preventDefault();
-    console.log(event);
-  };
+    const messageText = inputRef.current!.value;
+    if (messageText && username) {
+      const message = {
+        sender: username,
+        text: messageText,
+      };
+      sendTextMessage(message);
+    }
+  }
 
   return (
     <section className="p-2 h-full answer-chat relative">
       <div className="decor-answer absolute">
         <h1>Respostas</h1>
       </div>
-      <div className="answer-chat-container">
-        <ul className="answer-list flex flex-col justify-start">
-          {answersMock.map((answer, index) => (
-            <li
-              className="answer"
-              key={index}
-            >
-              <span className="user-name">{answer.username}: {""}</span>
-              <span className="answer-chat-text">{answer.answer}</span>
-            </li>
-          ))}
+      <div className="">
+        <ul className="answer-chat-container flex flex-col justify-start overflow-y-scroll h-40">
+          {messages.length > 0 &&
+            messages.map((message: Message, index) => (
+              <li
+                className="chat-message"
+                key={index}
+              >
+                <span className="chat-text">{message.sender}: {""}</span>
+                <span className="chat-text">{message.text}</span>
+              </li>
+            ))}
         </ul>
       </div>
       <form
@@ -78,11 +73,12 @@ const AnswerChat = ({ socket, username }: chatChildrenProps) => {
         <input
           type="text"
           className="answer-input p-2 rounded-md flex flex-1"
-          placeholder="Responda aqui"
+          placeholder="Adivinhhe aqui"
           ref={inputRef}
         />
       </form>
     </section>
   );
 };
+
 export default AnswerChat;
