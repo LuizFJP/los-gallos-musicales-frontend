00bf2405edfe6465723e9c8b4005b308474868b2
Nicolas Debacher <ndebacher@gmail.com>
2023-11-15 11:51:01 -0300

    new line type

--- src/presentation/components/canvas/canvas.tsx
+++ src/presentation/components/canvas/canvas.tsx
@@ -15,11 +15,40 @@ export type CanvasProps = {
   roomName: string;
 };
 
+type point = {
+  x: number;
+  y: number;
+};
+
+type line = {
+  start: point;
+  end: point;
+};
+
+
 export const Canvas = ({ artist, socket, roomName, room }: CanvasProps) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const debouncedSave = useRef(
     debounce((nextValue) => saveCanvas(nextValue), 5000)
   ).current;
+
+  const drawLine = (context: CanvasRenderingContext2D, line: line) => {
+    if (!context) {
+      console.error("Contexto não encontrado no draw");
+      return;
+    }
+
+    context.lineJoin = 'round';
+    context.lineCap = 'round'; 
+    context.lineWidth = 5;
+    context.strokeStyle = "black";
+    context.lineWidth = 2;
+
+    context.beginPath();
+    context.moveTo(line.start.x, line.start.y);
+    context.lineTo(line.end.x, line.end.y);
+    context.stroke();
+  }
 
   const drawCircle = (context: CanvasRenderingContext2D, x: number, y: number) => {
     if (!context) {
@@ -56,31 +85,43 @@ export const Canvas = ({ artist, socket, roomName, room }: CanvasProps) => {
     startDraw(context2d);
 
     let isDrawing = false;
+    let startPoint: { x: number; y: number } | null = null;
 
-    socket?.on(DRAW_EVENT, (data: any) => {
-      drawCircle(context2d, data.x, data.y);
+    socket?.on(DRAW_EVENT, (line: line) => {
+      drawLine(context2d, line);
     });
 
     function handleMouseDown(event: MouseEvent) {
-      if (!canvas || !artist) return;
+      if (!canvas || !context2d) return;
 
       isDrawing = true;
       const rect = canvas.getBoundingClientRect();
       const offsetX = event.clientX - rect.left;
       const offsetY = event.clientY - rect.top;
-      drawCircle(context2d as CanvasRenderingContext2D, offsetX, offsetY);
-      send(offsetX, offsetY);
+
+      startPoint = { x: offsetX, y: offsetY };
     }
 
     function handleMouseMove(event: MouseEvent) {
-      if (!isDrawing || !canvas || !artist) return;
+      if (!isDrawing || !canvas || !context2d) return;
+
       const rect = canvas.getBoundingClientRect();
       const offsetX = event.clientX - rect.left;
       const offsetY = event.clientY - rect.top;
-      drawCircle(context2d as CanvasRenderingContext2D, offsetX, offsetY);
-      send(offsetX, offsetY);
-    }
 
+      setTimeout(() => {
+        if (!startPoint) return;
+
+        const endPoint = { x: offsetX, y: offsetY };
+        const line = { start: startPoint, end: endPoint };
+
+        drawLine(context2d, line);
+        send(line);
+
+        startPoint = endPoint;
+      }, 20);
+    }
+    
     function handleMouseUp() {
       isDrawing = false;
     }
@@ -107,8 +148,8 @@ export const Canvas = ({ artist, socket, roomName, room }: CanvasProps) => {
     );
   };
 
-  const send = (x: number, y: number) => {
-    const data = { x, y };
+  const send = (line: line) => {
+    const data = line;
     socket?.emit(DRAW_EVENT, roomName, data, (error) => {
       if (error) {
         console.error('Error sending drawing:', error);
