29b77a091b5cc8970774aa45865b0ae88254e2ef
Gustavo Avelar <16326176760@edu.udesc.br>
2023-11-21 11:22:58 -0300

    creating before join shared room

---
+++ src/presentation/pages/join-room/join-room.tsx
@@ -0,0 +1,18 @@
+export const JoinRoom = () => {
+  return (
+    <main>
+      <header>
+        <h1>Você está prestes a entrar na sala {roomName}</h1>
+      </header>
+      <section>
+        <div>
+          Escolha uma imagem de perfil
+          <img src="" alt="" />
+        </div>
+        <h2>Escolha um nome de usuário</h2>
+        <input type="text" />
+        <button type="button">Entrar</button>
+      </section>
+    </main>
+  )
+}
