e3a16849a9803da178eb6e100f690bfa2cded0a5
solange-d <solange.duemes@edu.udesc.br>
2023-11-25 17:48:41 -0300

    Desenvolvimento caso de uso 11

--- src/presentation/components/lists/player-list/player-list.tsx
+++ src/presentation/components/lists/player-list/player-list.tsx
@@ -1,29 +1,78 @@
-import { Player } from "../../../../domain/entities/room/room"
-import Avatar from "../../../../assets/avatars/avatar_01.png"
+// player-list.tsx
 
+import React, { useState } from "react";
+import { Player } from "../../../../domain/entities/room/room";
+import Avatar from "../../../../assets/avatars/avatar_01.png";
 import "./player-list.scss";
+import { ActionModal } from "../../modal/action-modal/action-modal";
+import { MdReport } from "react-icons/md";
 
 export type PlayerListProps = {
-  players: Player[]
-}
+  players: Player[];
+};
 
 export const PlayerList = (props: PlayerListProps) => {
-  console.log(props.players)
+  const [selectedPlayer, setSelectedPlayer] = useState<Player | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = (player: Player) => {
+    setSelectedPlayer(player);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedPlayer(null);
+    setIsModalOpen(false);
+  };
+
   return (
     <section className="player-list-container">
+      {selectedPlayer && (
+        <div className="action-modal-overlay absolute">
+          <ActionModal
+            isOpen={isModalOpen}
+            title={`Denunciar ${selectedPlayer.username}`}
+            description={`Deseja denunciar o jogador ${selectedPlayer.username} por comportamento inadequado?`}
+            confirmText="Denunciar"
+            hasCancel
+            cancelText="Cancelar"
+            onConfirm={() => {
+              // Lógica de denúncia aqui
+              handleCloseModal();
+            }}
+            onCancel={handleCloseModal}
+            icon={MdReport} 
+            iconColor="#ff0000"
+          />
+        </div>
+      )}
       <ul className="player-list flex flex-col gap-4 overflow-y-scroll rounded-lg p-6">
-      {props.players.map((player, index) => (
-        <li key={index} className="flex items-center justify-start gap-4 hover:bg-gray-300 p-2 rounded-lg hover:text-gray-950 text-sm lg:text-lg text-gray-50">
-          <div className="avatar-container">
-            <img src={Avatar} alt="" className="selected-avatar rounded-full object-cover h-24 w-24 borer-solid border-4 border-gray-50"/>
-          </div>
-          <div className="player-info flex flex-col gap-2 ">
-          <span className="">{player.username}</span>
-          <span className="">{player.score} pts</span>
-          </div>
-        </li>
-      ))}
+        {props.players.map((player, index) => (
+          <li
+            key={index}
+            className="flex items-center justify-start gap-4 hover:bg-gray-300 p-2 rounded-lg hover:text-gray-950 text-sm lg:text-lg text-gray-50"
+          >
+            <div className="avatar-container">
+              <img
+                src={Avatar}
+                alt=""
+                className="selected-avatar rounded-full object-cover h-24 w-24 border-solid border-4 border-gray-50"
+              />
+            </div>
+            <div className="player-info flex flex-col gap-2">
+              <span>{player.username}</span>
+              <span>{player.score} pts</span>
+              <button
+                className="text-red-500 hover:underline"
+                onClick={() => handleOpenModal(player)}
+              >
+                Denunciar
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
+
     </section>
-  )
-}
+  );
+};
